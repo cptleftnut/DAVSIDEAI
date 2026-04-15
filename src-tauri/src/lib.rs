5f0fbc91ff658a220a93bf2e012f4018d520f6f4
cptleftnut <78494323+cptleftnut@users.noreply.github.com>
2026-04-15 20:50:35 +0200

    Update lib.rs
    Signed-off-by: cptleftnut <78494323+cptleftnut@users.noreply.github.com>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,11 @@
 // src-tauri/src/lib.rs
-
-pub fn greet(name: &str) -> String { format!("Hello, {}!", name)
-
-                                   }
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .setup(|_app| {
+            // Din eventuelle setup-kode her
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
